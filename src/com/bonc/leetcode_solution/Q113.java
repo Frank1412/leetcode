1249f04cb5a5086a39f446ab8052c6873a00ef57
fangshouzhi <fangshouzhi@bonc.com.cn>
2021-02-23 17:57:31 +0800

    113 start

---
+++ src/com/bonc/leetcode_solution/Q113.java
@@ -0,0 +1,20 @@
+package com.bonc.leetcode_solution;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @Author: Frank
+ * @Date: 2021-02-23 17:44
+ * @Description:
+ * Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
+ *
+ * A leaf is a node with no children.
+ *
+ */
+public class Q113 {
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+
+        return new LinkedList<>();
+    }
+}
