d92e2967afd820c2f4cec3034afb0626d34201b7
frank <826389969@qq.com>
2021-02-24 21:47:16 +0800

    Q450 递归解法

---
+++ src/com/bonc/leetcode_solution/Q450.java
@@ -0,0 +1,48 @@
+package com.bonc.leetcode_solution;
+
+/**
+ * @author: Shouzhi Fang(frank)
+ * @create: 2021-02-24 18:54
+ * @description:
+ * Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
+ *
+ * Basically, the deletion can be divided into two stages:
+ *
+ * Search for a node to remove.
+ * If the node is found, delete the node.
+ * Follow up: Can you solve it with time complexity O(height of tree)?
+ *
+ */
+public class Q450 {
+    public TreeNode deleteNode(TreeNode root, int key) {
+
+        // 递归解法
+        if (root==null) return null;
+        if (root.val > key){
+            root.left = deleteNode(root.left, key);
+        }else if (root.val < key){
+            root.right = deleteNode(root.right, key);
+        }else{
+            if (root.left==null&&root.right==null){
+                return null;
+            }
+            if (root.left==null){
+                return root.right;
+            }
+            if (root.right==null){
+                return root.left;
+            }
+            TreeNode leftMax = findLeftMax(root.left);
+            root.val = leftMax.val;
+            root.left = deleteNode(root.left, root.val);
+        }
+        return root;
+    }
+
+    public TreeNode findLeftMax(TreeNode root){
+        while (root.right!=null){
+            root = root.right;
+        }
+        return root;
+    }
+}
