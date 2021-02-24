56a9028c4c65f1c635b726b3a9c25510b87e09ef
frank <826389969@qq.com>
2021-02-24 22:47:08 +0800

    Q508 start

---
+++ src/com/bonc/leetcode_solution/Q508.java
@@ -0,0 +1,14 @@
+package com.bonc.leetcode_solution;
+
+/**
+ * @author: Shouzhi Fang(frank)
+ * @create: 2021-02-24 21:48
+ * @description:
+ * Given the root of a tree, you are asked to find the most frequent subtree sum. The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself). So what is the most frequent subtree sum value? If there is a tie, return all the values with the highest frequency in any order.
+ *
+ */
+public class Q508 {
+    public int[] findFrequentTreeSum(TreeNode root) {
+        return new int[2];
+    }
+}
