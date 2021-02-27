4969993f80f0f8b889c7d58bfb5162a8712708fb
frank <826389969@qq.com>
2021-02-27 17:06:41 +0800

    Q508 finished

--- src/com/bonc/leetcode_solution/Q508.java
+++ src/com/bonc/leetcode_solution/Q508.java
@@ -1,5 +1,11 @@
 package com.bonc.leetcode_solution;
 
+import java.security.Key;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author: Shouzhi Fang(frank)
  * @create: 2021-02-24 21:48
@@ -8,7 +14,40 @@ package com.bonc.leetcode_solution;
  *
  */
 public class Q508 {
+    int max = 0;
+    Map<Integer, Integer> set = new HashMap<>();
     public int[] findFrequentTreeSum(TreeNode root) {
-        return new int[2];
+        if (root==null){
+            return new int[]{};
+        }
+        subTreeSum(root);
+
+        System.out.println(set);
+        ArrayList<Integer> res = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry: set.entrySet()){
+            if (entry.getValue()==max) res.add(entry.getKey());
+        }
+        int[] ans = new int[res.size()];
+        int i=0;
+        while (i<res.size()){
+            ans[i] = res.get(i);
+            i++;
+        }
+        return ans;
+    }
+
+    public int subTreeSum(TreeNode root){
+        if (root.left==null && root.right==null){
+            set.put(root.val, set.getOrDefault(root.val, 0)+1);
+            max = Math.max(max, set.get(root.val));
+            return root.val;
+        }
+
+        int left = root.left!=null?subTreeSum(root.left):0;
+        int right = root.right!=null?subTreeSum(root.right):0;
+        int sum = left + right + root.val;
+        set.put(sum, set.getOrDefault(sum, 0)+1);
+        max = Math.max(max, set.get(sum));
+        return sum;
     }
 }
